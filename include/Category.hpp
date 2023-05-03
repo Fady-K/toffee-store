7d6713dd484ebe6678660961054b452420d8ff8b
Fady-K <popfadykamal151617@gmail.com>
2023-05-03 13:37:43 +0300

    added Category class header file

--- include/Category.hpp
+++ include/Category.hpp
@@ -7,8 +7,49 @@ using namespace std;
 class Category
 {
 private:
-    /* data */
+    /* static attributes (related to class only, and have nothing to do with instance) */
+    static int s_categoriesCount;
+
+    /* Instance attributes */
+    string m_name; 
+    int n_products;
+    Product* t_products;
+
+        
 public:
-    Category(/* args */);
+    /* Constructors and Destructor */
+    Category();                                         // Default contructor
+    Category(const string& t_name);                                         // Parametrized constructor
+    Category(const string& t_name, Product* t_products);
     ~Category();
+
+
+    /* Setters And Getters (encapsulation) */
+    void SetName(const string& t_newName);
+    string GetName() const;
+    int SetNumberOfProducts(const int n);
+    int GetNumberOfProducts() const;
+    Product* GetAllProducts() const;
+
+
+    /* Instance Methdos */
+    Product AddProduct(const string& t_name, const string& t_category, const string& t_descr, double t_price, const int t_amount);
+    Product RemoveProduct(const string& t_name);
+    void SortProducts(const bool ascendingOrder=true);
+    Product SearchProductByName(const string& t_name);
+    void DisplayAllProducts() const;
+    bool IsEmpty() const;
+    void Clear();
+
+    /* Operators overloading */
+    friend ostream& operator<< (ostream& output, const Category& t_cat);
+
+
+    /* static Methods (related to class only, and has nothing with instance) */
+    static int GetAllNumberOfCategories();
+
+
+private:
+    /* Helper Functions */
+
 };
