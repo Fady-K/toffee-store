a71191f61eb3c6e7e5140e97aa09d45661e45962
Fady-K <popfadykamal151617@gmail.com>
2023-05-10 14:43:56 +0300

    created shippinginfo class and implemented it

---
+++ include/ShippingInfo.hpp
@@ -0,0 +1,45 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "../src/Utility.cpp"
+#define endl "\n"
+using namespace std;
+
+class ShippingInfo
+{
+private:
+    string _recipientName;
+    string _recipientPhoneNumber;
+    string _city;
+    string _email; 
+    string _shippingAddress;
+
+
+public:
+    ShippingInfo();                         // Default constructor
+    ShippingInfo(string t_recipientName="", string t_recipientPhoneNumber="", string t_city="", string t_email="", string t_shippingAddress="");                         // Parametrized constructor
+    ~ShippingInfo();                        // Destructor
+
+
+    /* Setters And Getters (encapsulation) */
+    void SetRecipientName(const string& t_name);
+    void SetRecipientPhoneNumber(const string& t_phoneNumber);
+    void SetCity(const string& t_city);
+    void SetEmail(const string& t_email);
+    void SetShippingAddress(const string& t_address);
+
+    string GetRecipientName() const;
+    string GetRecipientPhoneNumber() const;
+    string GetCity() const;
+    string GetEmail() const;
+    string GetShippingAddress() const;
+
+    /* Instance Methods */
+    void print() const;
+
+    /* Operator overloading */
+    friend ostream& operator<< (ostream& output, const ShippingInfo& t_info);
+
+
+};
+
---
+++ src/ShippingInfo.cpp
@@ -0,0 +1,131 @@
+#include "../include/ShippingInfo.hpp"
+
+///////////////////////////////////////////// constructors and destructor ///////////////
+ShippingInfo::ShippingInfo(): _recipientName("None"), _recipientPhoneNumber("None"), _email("None"), _city("None"), _shippingAddress("None"){}
+
+ShippingInfo::ShippingInfo(string t_recipientName, string t_recipientPhoneNumber,  string t_email, string t_city, string t_shippingAddress)
+{
+    SetRecipientName(t_recipientName);
+    SetRecipientPhoneNumber(t_recipientPhoneNumber);
+    SetEmail(t_email);
+    SetCity(t_city);
+    SetShippingAddress(t_shippingAddress);
+}
+
+ShippingInfo::~ShippingInfo(){}
+
+
+/////////////////////////////////////////// Setters and getters ///////////////////
+
+
+void ShippingInfo::SetRecipientName(const string& t_name)
+{
+    if (Utility::isValidName(t_name))
+    {
+        this->_recipientName = t_name;
+    }
+    else
+    {
+        throw invalid_argument("!! Not A Valid Name !!");
+    }
+}
+
+void ShippingInfo::SetRecipientPhoneNumber(const string& t_phoneNumber)
+{
+    if (Utility::isValidPhoneNumber(t_phoneNumber))
+    {
+        this->_recipientPhoneNumber = t_phoneNumber;
+        cout << "true" << endl;
+    }
+    else
+    {
+        throw invalid_argument("!! Not A Valid PhoneNumber !!");
+    }
+}
+
+void ShippingInfo::SetEmail(const string& t_email)
+{
+    // if (Utility::isValidEmail(t_email))
+    if (true)
+    {
+        this->_email = t_email;
+    }
+    else
+    {
+        throw invalid_argument("!! Not A Valid Email !!");
+    }
+}
+
+void ShippingInfo::SetCity(const string& t_city)
+{
+    // if (Utility::isValidAddress(t_city))
+    if (true)
+    {
+        this->_city = t_city;
+    }
+    else
+    {
+        throw invalid_argument("!! Not A Valid City !!");
+    }
+}
+
+void ShippingInfo::SetShippingAddress(const string& t_address)
+{
+    // if (Utility::isValidAddress(t_address))1
+    if (true)
+    {
+        this->_shippingAddress = t_address;
+    }
+    else
+    {
+        throw invalid_argument("!! Not A Valid Address !!");
+    }
+}
+
+
+string ShippingInfo::GetRecipientName() const
+{
+    return this->_recipientName;
+}
+
+string ShippingInfo::GetRecipientPhoneNumber() const
+{
+    return this->_recipientPhoneNumber;
+}
+
+string ShippingInfo::GetEmail() const
+{
+    return this->_email;
+}
+
+string ShippingInfo::GetCity() const
+{
+    return this->_city;
+}
+
+string ShippingInfo::GetShippingAddress() const
+{
+    return this->_shippingAddress;
+}
+
+
+/////////////////////////////////////////// Instance Methods /////////////////////////
+void ShippingInfo::print() const
+{
+    cout << "----------------------------------------- " << this->_recipientName << " Shipping Information -------------------------------" << endl;
+    cout << "RecipientName: " << _recipientName << "\n" << "RecipientPhoneNumber: " << this->_recipientPhoneNumber << endl;
+    cout << "RecipientEmail: " << _email << "\n" << "City: " << this->_city << endl;
+    cout << "Address: " << this->_shippingAddress << endl;
+}
+
+
+/////////////////////////////////////// Operator overloading ////////////////////
+ostream& operator<< (ostream& output, const ShippingInfo& t_info)
+{
+    output << "----------------------------------------- " << t_info.GetRecipientName() << " Shipping Information -------------------------------" << endl;
+    output << "RecipientName: " << t_info.GetRecipientName() << "\n" << "RecipientPhoneNumber: " <<t_info.GetRecipientPhoneNumber() << endl;
+    output << "RecipientEmail: " << t_info.GetEmail() << "\n" << "City: " << t_info.GetCity() << endl;
+    output << "Address: " <<t_info.GetShippingAddress() << endl;
+
+    return output;
+}
