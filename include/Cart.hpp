5c63b3da0448af20431677e85df09d5932973e32
Fady-K <popfadykamal151617@gmail.com>
2023-05-10 11:12:38 +0300

    modified

--- include/Cart.hpp
+++ include/Cart.hpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include "../src/Utility.cpp"
 #include "../src/Product.cpp"
 using namespace std;
 
@@ -8,6 +9,7 @@ class Cart
 {
 private:
     string m_cartId;
+    double m_subTotal;
     vector<Product> m_goods;
     
 public:
@@ -22,9 +24,14 @@ public:
     /* Instance Methods */
     Product AddToCart(Product t_product, const int t_quantity = 0);
     Product RemoveFromCart(const string& t_productName);
+    bool IncreaseProductAmount(const string& t_productName, const int t_amount = 0);
+    bool DecreaseProductAmount(const string& t_productName, const int t_amount = 0);
+    pair<bool, Product*> exists(const string& t_productName);
+    double TotalPrice() const;
     void Clear();
     void print() const;
     bool isEmpty() const;
+
 
 };
 
--- src/Cart.cpp
+++ src/Cart.cpp
@@ -23,6 +23,9 @@ Product Cart::AddToCart(Product t_product, const int quntity)
     // set product quantity as quantity he wants to puy
     t_product.SetAmount(quntity);
 
+    // add to total price
+    m_subTotal += (t_product.GetPrice() * quntity);
+
     // append to cart
     m_goods.push_back(t_product);
     return t_product;
@@ -43,7 +46,12 @@ Product Cart::RemoveFromCart(const string& t_productName)
         if (it != m_goods.end())
         {
             // remove from cart
-            Product tmp = *it;
+            Product tmp = *it;          // store copy to return
+
+            // decrement subTotal
+            m_subTotal -= (it->GetPrice() * it->GetAvailableAmount());
+
+            // remove item from cart
             m_goods.erase(it);
 
             return tmp;
@@ -54,6 +62,126 @@ Product Cart::RemoveFromCart(const string& t_productName)
             return Product();
         }
     }
+}
+
+pair<bool, Product*> Cart::exists(const string& t_productName)
+{
+    pair<bool, Product*> ans;
+
+    auto it = find_if(m_goods.begin(), m_goods.end(),
+                      [&](const Product& p){ return p.GetProductName() == t_productName; });
+
+    if (it != m_goods.end())
+    {
+        // Product exists in cart
+        ans.first = true;
+        ans.second = &(*it);
+    }
+    else
+    {
+        ans.first = false;
+        ans.second = nullptr;
+    }
+
+    return ans;
+}
+
+
+bool Cart::IncreaseProductAmount(const string& t_productName, const int t_amount)
+{
+    // flag
+    bool hasIncreased = false;
+
+    // check if valid amount
+    if (t_amount >= 0)
+    {
+        /* note on exists: 
+
+            returns pair where, pair.first boolian value indicating whether item exists or not, 
+            pair.second returns a pointer to the product soThat user be able to increase it.
+
+        */
+
+        // get pair
+        pair<bool, Product*> existsAndProduct =  exists(t_productName);
+
+        if (existsAndProduct.first)
+        {
+            // increase product amount
+            existsAndProduct.second->IncreaseAmount(t_amount);
+
+            // update total price
+            m_subTotal += (existsAndProduct.second->GetPrice() * t_amount);
+
+            // switch flag
+            hasIncreased = true;
+        }
+        else
+        {
+            cout << "!! No Such Product: " << t_productName << " Exists In Cart !!" << endl;
+        }
+    }
+    else
+    {
+        cout << "!! Invalid Amount !!" << endl;
+    }
+
+    // return flag
+    return hasIncreased;
+}
+
+bool Cart::DecreaseProductAmount( const string& t_productName, const int t_amount)
+{
+    // flag
+    bool hasDecreeased = false;
+
+    // check if valid amount
+    if (t_amount >= 0)
+    {
+        /* note on exists: 
+
+            returns pair where, pair.first boolian value indicating whether item exists or not, 
+            pair.second returns a pointer to the product soThat user be able to increase it.
+
+        */
+
+        // get pair
+        pair<bool, Product*> existsAndProduct =  exists(t_productName);
+
+        if (existsAndProduct.first)
+        {
+            // increase product amount
+           if (existsAndProduct.second->DecreaseAmount(t_amount))
+           {
+                // update total price
+               m_subTotal -= (existsAndProduct.second->GetPrice() * t_amount);
+
+               
+                // switch flag
+                hasDecreeased = true;
+           }
+
+        }
+
+        else
+        {
+            cout << "!! No Such Product: " << t_productName << " Exists In Cart !!" << endl;
+        }
+    }
+    else
+    {
+        cout << "!! Invalid Amount !!" << endl;
+    }
+
+    // return flag
+    return hasDecreeased;
+}
+
+
+
+double Cart::TotalPrice() const
+{
+    return this->m_subTotal;
 }
 
 void Cart::Clear()
@@ -84,12 +212,14 @@ void Cart::print() const
     {
         cout << "--------------------------- Cart -------------------" << endl;
         cout << "CartId: " << GetId() << "\n";
-        cout << "Products In Cart:" << endl;
+        cout << endl;
 
         for (auto it = m_goods.begin(); it != m_goods.end(); ++it)
         {
             cout << *it;
         }
+
+        cout << "TotalPrice: " << this->m_subTotal << '$' << endl;
     }
     
 }
