e34f27ffdc4a1e6e69f337f1a1a2e6a0ff5079cf
Fady-K <popfadykamal151617@gmail.com>
2023-05-10 16:59:31 +0300

    empty

---
+++ include/Customer.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "../src/User.cpp"
+
+class Customer: public User
+{
+private:
+
+public:
+    Customer();             // Default constructor
+    Customer();             // Parametrized constructor
+    ~Customer();            // Destructor
+
+
+    /* Setter & Getters */
+
+
+    /* Insatnce Methods */
+
+};
+
---
+++ src/Customer.cpp
@@ -0,0 +1,1 @@
+#include "../include/Customer.hpp"
